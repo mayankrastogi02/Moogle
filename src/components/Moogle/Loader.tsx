f5f6d5b288466687a895eb99150bf290b212be67
Mayank Rastogi <mayankrustey2002@gmail.com>
2023-09-14 19:05:58 -0700

    Fixed build errors, ready to deply v2

--- src/components/Moogle/Loader.tsx
+++ src/components/Moogle/Loader.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = ({ number }: { number: number }) => {
     return (
         Array(number).fill(0).map((el, index) => (
-            <div className='flex flex-col justify-center md:mx-[184px] p-4 gap-6'>
+            <div className='flex flex-col justify-center md:mx-[184px] p-4 gap-6' key={el + "-" + index}>
                 <div className='max-w-3xl' key={index}>
                     <div className="flex flex-row items-center">
                         <div className='w-10 h-10 rounded-full bg-mgrey m-2 flex items-center justify-center animate-pulse' />
