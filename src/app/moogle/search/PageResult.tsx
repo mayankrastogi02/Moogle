a79198301b4840129351d2abc5537ef04b8133f6
Mayank Rastogi <mayankrustey2002@gmail.com>
2023-09-23 11:35:44 -0700

    Converted results to client component

--- src/app/moogle/search/PageResult.tsx
+++ src/app/moogle/search/PageResult.tsx
@@ -1,17 +1,35 @@
+'use client';
 import { rankPages } from '@/Hooks/rankPages';
+import Loader from '@/components/Moogle/Loader';
 import { AlsoSearch } from '@/components/Moogle/Search';
 import NothingFound from '@/components/Moogle/Search/NothingFound';
 import Result from '@/components/Moogle/Search/Result';
 import SearchResults from '@/types/pages';
 import { getPages } from '@/utils/dbFunctions';
-import React from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 
-const PageResult = async ({ query }: { query: string }) => {
-    setTimeout(() => {
-        console.log("Delayed for 1 second.");
-    }, 5000);
-    const unrankedData: SearchResults[] = await getPages(query);
-    const data = rankPages(unrankedData, query);
+const PageResult = ({ query }: { query: string }) => {
+    const [data, setData] = useState<SearchResults[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    useEffect(() => {
+        const fetchData = async () => {
+            const unrankedData: SearchResults[] = await getPages(query);
+            const data = rankPages(unrankedData, query);
+            setData(data);
+            setLoading(false);
+        }
+        fetchData();
+        // const unrankedData: SearchResults[] = await getPages(query);
+        // const data = rankPages(unrankedData, query);
+        // setData(data);
+    }, []);
+
+    if (loading) {
+        return (
+            <Loader number={4} />
+        )
+    }
+
     return (
         <div className='flex flex-col justify-center md:mx-[184px] p-4 gap-6'>
             <p className='text-md text-slate-400 font-[500]'>About {data.length} results (it happened very fast!)</p>
@@ -22,7 +40,7 @@ const PageResult = async ({ query }: { query: string }) => {
                             <Result key={index} result={item} />
                         )
                     })
-                    : <NothingFound />
+                    : !loading ? <NothingFound /> : null
             }
             {
                 data.length > 0 ? <AlsoSearch /> : null
--- src/app/moogle/search/page.tsx
+++ src/app/moogle/search/page.tsx
@@ -11,15 +11,15 @@ type Props = {
     searchParams: { q: string }
 }
 
-// export async function generateMetadata({ params, searchParams }: Props,
-//     parent: ResolvingMetadata
-// ): Promise<Metadata> {
-//     return {
-//         title: `${searchParams.q} - Moogle`,
-//         description: `Search results for ${searchParams.q} on Moogle`,
-//         icons: "/m_yellow.svg"
-//     };
-// }
+export async function generateMetadata({ params, searchParams }: Props,
+    parent: ResolvingMetadata
+): Promise<Metadata> {
+    return {
+        title: `${searchParams.q} - Moogle`,
+        description: `Search results for ${searchParams.q} on Moogle`,
+        icons: "/m_yellow.svg"
+    };
+}
 
 const Page = ({ searchParams }: { searchParams: { q: string } }) => {
     const query: string | null = searchParams ? searchParams.q : null;
@@ -30,10 +30,10 @@ const Page = ({ searchParams }: { searchParams: { q: string } }) => {
         <div className='min-h-screen'>
             <Navbar query={query} />
             <hr className='h-px border-1 border-mgrey' />
-            <Suspense fallback={<Loader number={4} />}>
-                <PageResult query={query} />
-                {/* <Test /> */}
-            </Suspense>
+            {/* <Suspense fallback={<Loader number={4} />}> */}
+            <PageResult query={query} />
+            {/* <Test /> */}
+            {/* </Suspense> */}
         </div>
     )
 }
